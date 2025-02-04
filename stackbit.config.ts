6c0af1de6a2907a737e524ae7d13ac3abf6ff705
Aarnav Lokesh <106572373+Aarnav-Tech@users.noreply.github.com>
2025-02-04 20:19:32 +0530

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,8 +1,24 @@
-import { defineStackbitConfig } from '@stackbit/types';
-import { GitContentSource } from '@stackbit/cms-git';
 import { allModels } from './.stackbit/models';
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
 
 const config = defineStackbitConfig({
+    contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Page",
+          // Define the model as a page model
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true }]
+        }
+      ],
+    })
+  ]
     stackbitVersion: '~0.7.0',
     ssgName: 'nextjs',
     nodeVersion: '18',
